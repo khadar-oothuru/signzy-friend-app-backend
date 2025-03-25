96f35ede6cb125a9fd4c3c27cbc64aeb40efc4c3
khadar-oothuru <khadaroothuru@gmail.com>
2025-03-25 11:41:45 +0530

    updated json file

--- config/db.js
+++ config/db.js
@@ -1,15 +1,12 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log('MongoDB Connected');
-    } catch (error) {
-        console.error('MongoDB connection error:', error);
-        setTimeout(connectDB, 5000);  
+        console.log('MongoDB connected');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
     }
 };
 
